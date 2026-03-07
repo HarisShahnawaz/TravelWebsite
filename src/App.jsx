75503fc15fb1543c12f5194dabec0c4c4fbf3cfb
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-07 21:30:54 +0500

    add pages functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./routes/Contact";
+import Service from "./routes/Service";
+import { MenuItems } from "./components/MenuItems";
+import About from "./routes/About";
 function App() {
   return (
-    <div className="app">
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
       <Navbar />
     </div>
   );
---
+++ src/routes/Service.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Service() {
+  return (
+    <>
+      <h1>this is service</h1>
+    </>
+  );
+}
+
+export default Service;
