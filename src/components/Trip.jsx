07b8d922901064fd6a103a013a3b77bbb208c4f5
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-09 15:33:19 +0500

    fix

---
+++ src/components/Trip.jsx
@@ -0,0 +1,12 @@
+import "./Trip.css";
+
+function Trip() {
+  return (
+    <div className="trip">
+      <h1>Recent Trips</h1>
+      <p>You Can Discover unique Destinations using Google Maps.</p>
+    </div>
+  );
+}
+
+export default Trip;
---
+++ src/components/TripData.jsx
@@ -0,0 +1,9 @@
+import "./Trip.css";
+
+function TripData() {
+  return (
+    <div className="t-card">
+      <div className="t-image"></div>
+    </div>
+  );
+}
